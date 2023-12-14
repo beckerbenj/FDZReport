14c45567970be4fb425be5836d3244ef91e114dd
Benjamin Becker <benj.becker@web.de>
2023-12-14 15:17:09 +0100

    fix cfs

--- R/prepare_and_extract_CFs.R
+++ R/prepare_and_extract_CFs.R
@@ -11,11 +11,13 @@
 #'
 #'@export
 prepare_and_extract_cfs <- function(cfs_uebersicht) {
-  cfs_ori <- readxl::read_xlsx(cfs_uebersicht, sheet = "CF-applications")
-  cfs <- cfs_ori[is.na(cfs_ori$`abgelehnt am`), c("genehmigt am", "Studien")]
+  cfs_ori <- readxl::read_xlsx(cfs_uebersicht, sheet = "CF-applications",
+                              col_types = c("text", "text", "date", "date", "date", "text", "date", "text", "date",
+                                            "text", "date", "date", "numeric", "text"))
+  cfs <- cfs_ori[is.na(cfs_ori$`abgelehnt am`), c("Antragsdatum", "Studien")]
 
   #table(studien_ori$Status)
-  cfs$Jahr <- as.numeric(substr(cfs$`genehmigt am`, 7, 10))
+  cfs$Jahr <- as.numeric(substr(cfs$Antragsdatum, 1, 4))
 
   stopifnot(all(!is.na(cfs$Jahr)))
   cfs
