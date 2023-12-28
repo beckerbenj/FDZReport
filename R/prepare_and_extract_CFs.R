ab41297ff6f8c598001b8151e9eeed1a94fcd2f6
Benjamin Becker <benj.becker@web.de>
2023-12-28 12:52:56 +0100

    fix campus files for xlsx

--- R/prepare_and_extract_CFs.R
+++ R/prepare_and_extract_CFs.R
@@ -11,12 +11,24 @@
 #'
 #'@export
 prepare_and_extract_cfs <- function(cfs_uebersicht) {
-  suppressWarnings(cfs_ori <- readxl::read_xlsx(cfs_uebersicht, sheet = "CF-applications",
-                              col_types = c("text", "text", "date", "date", "date", "text", "date", "text", "date",
-                                            "text", "date", "date", "numeric", "text")))
-  cfs <- cfs_ori[is.na(cfs_ori$`abgelehnt am`), c("Antragsdatum", "Studien")]
+  ### someday: allow .csv import as this is the export format of the data base (via browser)
 
-  #table(studien_ori$Status)
+  # continue: seems to be a problem with "; in line 104?
+  #cfs_ori <- read.csv2(cfs_uebersicht, row.names = NULL,
+  #                     colClasses = c(NULL, NULL, "character", "character", "character", "character",
+  #                                                 "character", "character", "character",
+  #                                                "character", "character", "character", "character", "character"))
+  #cfs <- cfs_ori[is.na(cfs_ori$`abgelehnt.am`) | cfs_ori$`abgelehnt.am` == "", c("Antragsdatum", "Studien")]
+  #cfs <- cfs[cfs$Antragsdatum != "-", ]
+
+  suppressWarnings(cfs_ori <- readxl::read_excel(cfs_uebersicht, sheet = 1,
+                              col_types = c("text", "text", "date", "date", "date", "text",
+                                           "date", "text", "date",
+                                          "text", "date", "date", "numeric", "text")))
+  cfs <- cfs_ori[is.na(cfs_ori$`abgelehnt am`), c("Antragsdatum", "Studien")]
+  cfs <- cfs[!is.na(cfs$Antragsdatum), ]
+
+  #table(cfs$Studien)
   cfs$Jahr <- as.numeric(substr(cfs$Antragsdatum, 1, 4))
 
   stopifnot(all(!is.na(cfs$Jahr)))
